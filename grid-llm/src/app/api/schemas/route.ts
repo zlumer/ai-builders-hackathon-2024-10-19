92812fbc834b66452d548e67775741895363a52d
zlumer <zlumer@gmail.com>
2024-10-19 19:25:58 +0000

    fix: remove unused imports and code in route.ts

--- grid-llm/src/app/api/schemas/route.ts
+++ grid-llm/src/app/api/schemas/route.ts
@@ -1,18 +1,6 @@
-import { db } from '@/lib/db'
-import { sql } from "drizzle-orm"
 import { endpoint } from '@/utils/next/endpoint'
-
-const GET_SCHEMAS = sql`
-SELECT schema_name
-FROM information_schema.schemata
-WHERE 
-	schema_name NOT LIKE 'pg_%'
-	AND schema_name NOT IN ('information_schema', 'public', 'toolkit_experimental')
-	AND schema_name NOT LIKE '%timescaledb%'
-`
 
 export const GET = endpoint(async () =>
 {
-	let result = await db.execute(GET_SCHEMAS)
-	return result.rows.map(row => row.schema_name)
+	return "hello"
 })
--- grid-llm/src/app/hi/page.tsx
+++ grid-llm/src/app/hi/page.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import { db } from '@/lib/db'
+import { sql } from 'drizzle-orm'
 
-export default function HiPage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Hi there!
-        </h1>
-        <p className="mt-3 text-2xl">
-          Welcome to your new page.
-        </p>
-      </main>
-    </div>
-  );
+const GET_SCHEMAS = sql`
+SELECT schema_name
+FROM information_schema.schemata
+WHERE 
+	schema_name NOT LIKE 'pg_%'
+	AND schema_name NOT IN ('information_schema', 'public', 'toolkit_experimental')
+	AND schema_name NOT LIKE '%timescaledb%'
+`
+
+async function getSchemas(): Promise<string[]>
+{
+	let result = await db.execute(GET_SCHEMAS)
+	return result.rows.map(row => row.schema_name + "")
+}
+
+export default async function HiPage()
+{
+	let schemas = await getSchemas()
+
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+				<h1 className="text-6xl font-bold">
+					Hi there!
+				</h1>
+				<p className="mt-3 text-2xl">
+					Welcome to your new page.
+				</p>
+			</main>
+		</div>
+	);
 }
