ee06d56000842790254d9973ef0a1949eac86b30
zlumer <zlumer@gmail.com>
2024-10-19 19:21:39 +0000

    feat: Rename hello route to schemas route

--- grid-llm/src/app/api/schemas/route.ts
+++ grid-llm/src/app/api/schemas/route.ts
@@ -1,8 +1,6 @@
-import { users } from '@/drizzle/schema'
 import { db } from '@/lib/db'
-import { eq, sql } from "drizzle-orm"
+import { sql } from "drizzle-orm"
 import { endpoint } from '@/utils/next/endpoint'
-import { NextResponse } from 'next/server'
 
 const GET_SCHEMAS = sql`
 SELECT schema_name
@@ -17,13 +15,4 @@ export const GET = endpoint(async () =>
 {
 	let result = await db.execute(GET_SCHEMAS)
 	return result.rows.map(row => row.schema_name)
-
-	// let tables = await db.execute(sql`select * from information_schema.tables`)
-	// return tables
-	// const [user] = await db.select().from(users).limit(1)
-
-	// if (!user)
-	// 	throw NextResponse.json({ message: 'User not found' }, { status: 404 })
-
-	// return { user }
 })
