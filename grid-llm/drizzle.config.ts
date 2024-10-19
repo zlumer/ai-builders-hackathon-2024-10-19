2a44a0a9bdac04f6c598f9409626a00d45078a24
zlumer <zlumer@gmail.com>
2024-10-19 18:51:55 +0000

    chore: Update Drizzle configuration and file paths

--- grid-llm/drizzle.config.ts
+++ grid-llm/drizzle.config.ts
@@ -8,7 +8,7 @@ if (!process.env.DATABASE_URL)
 
 export default {
 	schema: './src/drizzle.schema.ts',
-	out: './drizzle',
+	out: './src/drizzle',
 	dialect: "postgresql",
 	dbCredentials: {
 		url: process.env.DATABASE_URL,
