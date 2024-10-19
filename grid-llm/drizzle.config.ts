308821c47738d32f5a0b7942ec928867c8570069
zlumer <zlumer@gmail.com>
2024-10-19 18:43:10 +0000

    chore: update drizzle config

--- grid-llm/drizzle.config.ts
+++ grid-llm/drizzle.config.ts
@@ -1,13 +1,17 @@
-import type { Config } from 'drizzle-kit';
-import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv'
+import type { Config } from 'drizzle-kit'
 
-dotenv.config();
+dotenv.config()
+
+if (!process.env.DATABASE_URL)
+	throw new Error('DATABASE_URL is not set')
 
 export default {
-	schema: './src/lib/schema.ts',
+	schema: './src/drizzle.schema.ts',
 	out: './drizzle',
-	driver: 'd1-http',
+	dialect: "postgresql",
 	dbCredentials: {
-		url: process.env.DATABASE_URL!,
-	},
-} satisfies Config;
+		url: process.env.DATABASE_URL,
+
+	}
+} satisfies Config
