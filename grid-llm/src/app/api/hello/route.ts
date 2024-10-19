0021716089fbf13be76c48113c8b397f97bb9645
zlumer <zlumer@gmail.com>
2024-10-19 19:20:51 +0000

    feat: Add endpoint to list database schemas

--- grid-llm/src/app/api/hello/route.ts
+++ grid-llm/src/app/api/hello/route.ts
@@ -1,15 +1,29 @@
 import { users } from '@/drizzle/schema'
 import { db } from '@/lib/db'
-import { eq } from "drizzle-orm"
+import { eq, sql } from "drizzle-orm"
 import { endpoint } from '@/utils/next/endpoint'
 import { NextResponse } from 'next/server'
 
+const GET_SCHEMAS = sql`
+SELECT schema_name
+FROM information_schema.schemata
+WHERE 
+	schema_name NOT LIKE 'pg_%'
+	AND schema_name NOT IN ('information_schema', 'public', 'toolkit_experimental')
+	AND schema_name NOT LIKE '%timescaledb%'
+`
+
 export const GET = endpoint(async () =>
 {
-	const [user] = await db.select().from(users).limit(1)
+	let result = await db.execute(GET_SCHEMAS)
+	return result.rows.map(row => row.schema_name)
 
-	if (!user)
-		throw NextResponse.json({ message: 'User not found' }, { status: 404 })
+	// let tables = await db.execute(sql`select * from information_schema.tables`)
+	// return tables
+	// const [user] = await db.select().from(users).limit(1)
 
-	return { user }
+	// if (!user)
+	// 	throw NextResponse.json({ message: 'User not found' }, { status: 404 })
+
+	// return { user }
 })
